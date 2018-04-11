7b53c4fd399ef7350ab8f8621f2b0058e6a370de
S4in7 <34082165+S4in7@users.noreply.github.com>
2018-04-11 03:43:02 -0700

    Add files via upload
    This is similar to 05Assignment but with a .txt file of data implanted into the chatbot

---
+++ csc370_RudeJ_06Assignment.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+#########################
+#       ChatBot v 1.0   #
+#       CSC370          #
+#       By: Jordon Rude #
+#########################
+#
+# This is a basic chat bot using the chatterbot API
+#
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+# Create a new chat bot named Frank
+chatbot = ChatBot('Frank',
+        storage_adapter="chatterbot.storage.SQLStorageAdapter",
+        logic_adapters=[
+                "chatterbot.logic.BestMatch"
+    ],
+        input_adapter="chatterbot.input.TerminalAdapter",
+        output_adapter="chatterbot.output.TerminalAdapter",
+        database="../database.db"
+)
+
+#this line sets the trainer to use the follow lines
+chatbot.set_trainer(ListTrainer)
+
+#each line is an acceptable answer to the previous line
+
+
+# import the text dump of data from .txt file for chatbot
+aesop = open('aesop.txt', 'r').readlines()
+
+chatbot.train(aesop)
+
+
+chatbot.train([
+        "Hi!",
+        "Hello!",
+        "Hey there!",
+        "Oh hi!",
+])
+
+chatbot.train([
+        "How are you?",
+        "I am fine.",
+        "That is good",
+        "I am glad",
+])
+
+#initiates code and asks for user input
+print("Type something to begin...")
+
+
+# The following loop will execute each time the user enters input
+while True:
+    try:
+        # We pass None to this method because the parameter
+        # is not used by the TerminalAdapter
+        chatbot_input = chatbot.get_response(None)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
+
+
+#####
+# more testing is needed and a better UI for seeing which 
+# is chatting, the bot or the user
+#
+#####
+
+
+
+
+
